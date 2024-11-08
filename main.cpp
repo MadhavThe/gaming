8cb030b62d8dfa418afe75263c98fb43dc3fa40e
MadhavThe <121456642+MadhavThe@users.noreply.github.com>
2024-11-08 16:55:38 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#define SDL_MAIN_HANDLED
+#include <windows.h>
+#include <SDL2/SDL.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("SDL Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
+    // Wait for 5 seconds
+    SDL_Delay(5000);
+
+    // Clean up
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
+//busefbkaeb
